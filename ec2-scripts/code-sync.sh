ffd5a5cfce033407233bdb5d6616b9e06b51f307
Ricardo Prado <ricardo.prado@simpli.com.br>
2018-05-07 16:52:45 -0300

    Code-sync fixes + startup script

--- ec2-scripts/code-sync.sh
+++ ec2-scripts/code-sync.sh
@@ -1,6 +1,11 @@
-rm -rf /home/ec2-user/temp/
-git clone $NODESRC /home/ec2-user/temp/
-mv -bfv /home/ec2-user/temp/* /home/ec2-user/neo-cli/
-find / -name "*~" -exec rm -rf {} \;
-/home/ec2-user/dotnet/dotnet restore /home/ec2-user/neo-cli/
-/home/ec2-user/dotnet/dotnet publish -c Release /home/ec2-user/neo-cli/
+export PROJECT_FOLDER=/home/ec2-user  
+export HOME=/home/ec2-user 
+rm -rf $PROJECT_FOLDER/temp
+git clone $NODESRC $PROJECT_FOLDER/temp/
+rm -rf $PROJECT_FOLDER/neo-cli/
+mkdir $PROJECT_FOLDER/neo-cli/
+mv -bfv $PROJECT_FOLDER/temp/* $PROJECT_FOLDER/neo-cli/
+find $PROJECT_FOLDER -name "*~" -exec rm -rf {} \;
+$PROJECT_FOLDER/dotnet/dotnet restore $PROJECT_FOLDER/neo-cli/
+$PROJECT_FOLDER/dotnet/dotnet publish -c Release $PROJECT_FOLDER/neo-cli/
+chown -R ec2-user:ec2-user $PROJECT_FOLDER
---
+++ ec2-scripts/user-data-startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export NODESRC=https://github.com/neo-project/neo-cli.git
+export BUCKETNAME=brcomsimpli
+/home/ec2-user/scripts/code-sync.sh
+
