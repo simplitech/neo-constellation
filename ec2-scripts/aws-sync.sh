6741574ce80103fb08c8a3e0bfe64a8cb13b3323
Ricardo Prado <ricardo.prado@simpli.com.br>
2018-05-07 18:04:06 -0300

    Utility commands for prototyping. Adjustments on aws-sync file

--- ec2-scripts/aws-sync.sh
+++ ec2-scripts/aws-sync.sh
@@ -1,1 +1,4 @@
-aws s3 sync s3://$BUCKETNAME/neo/config/ /home/ec2-user/neo-cli/neo-cli/ 2>&1  >>/home/ec2-user/crontaboutput.txt
+export PROJECT_FOLDER=/home/ec2-user
+export BUCKETNAME=brcomsimpli
+sudo aws s3 sync s3://$BUCKETNAME/neo/config/ $PROJECT_FOLDER/neo-cli/neo-cli/bin/Release/netcoreapp2.0/
+sudo chown -R ec2-user:ec2-user $PROJECT_FOLDER/neo-cli/neo-cli/
